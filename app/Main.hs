f5ef1cd52fb789301c01def6fe0684861b94303e
Tom Welsh <tswelsh@hotmail.com>
2017-07-03 15:27:47 +0100

    Remove compiler warnings

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,6 @@ import           Criterion.Main
 import           Data.Aeson
 import qualified Data.ByteString.Lazy        as BL
 import qualified Data.Text.Encoding          as T
-import qualified Data.Text.IO                as T
 
 testEvent :: BL.ByteString
 testEvent = BL.fromStrict $ T.encodeUtf8 "{\"stream\":\"actuals-stream\",\"submitter\":{\"type\":\"other\",\"description\":\"redactedredac\"},\"driverActivities\":[{\"driverActivity\":{\"journey\":{\"headcode\":\"1A01\",\"description\":\"redactedredactedredactedredactedredactedredacte\"},\"activity\":[{\"arrivalTime\":null,\"sequence\":1,\"tiploc\":\"REDACTE\",\"stop\":true,\"departureTime\":\"2016-06-09T18:22:28.000000000000Z\"},{\"arrivalTime\":\"2016-06-09T18:24:43.000000000000Z\",\"sequence\":2,\"tiploc\":\"REDACTE\",\"stop\":true,\"departureTime\":\"2016-06-09T18:25:51.000000000000Z\"},{\"arrivalTime\":\"2016-06-09T18:26:58.000000000000Z\",\"sequence\":3,\"tiploc\":\"REDACT\",\"stop\":true,\"departureTime\":\"2016-06-09T18:28:08.000000000000Z\"},{\"arrivalTime\":\"2016-06-09T18:29:57.000000000000Z\",\"sequence\":4,\"tiploc\":\"REDACTE\",\"stop\":true,\"departureTime\":null}]},\"activityUserId\":\"521be60a-02f2-4892-b468-c17d9c1c4fcf\"}],\"submissionTime\":\"2016-06-09T18:36:45.831486000000Z\",\"type\":\"driverActivityLogged\"}"
